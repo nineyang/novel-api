54ef31ba8ad642ff98e20ae9704226645ac9202d
nine <nine.yang.coding@gmail.com>
2018-12-21 22:05:33 +0800

    init something

---
+++ src/main/java/top/hellonine/novel/novelapi/entity/BookEntity.java
@@ -0,0 +1,14 @@
+package top.hellonine.novel.novelapi.entity;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * User: Nine
+ * Date: 2018/12/21
+ * Time: 下午5:24
+ */
+
+@Mapper
+public class BookEntity {
+
+}
---
+++ src/main/java/top/hellonine/novel/novelapi/mapper/BookMapper.java
@@ -0,0 +1,12 @@
+package top.hellonine.novel.novelapi.mapper;
+
+/**
+ * User: Nine
+ * Date: 2018/12/21
+ * Time: 下午5:23
+ */
+
+
+public class BookMapper {
+
+}
